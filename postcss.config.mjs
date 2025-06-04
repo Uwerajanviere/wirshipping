fc7d0612634a69926d72236fec83b9558e3fb6be
Uwerajanviere <124252344+Uwerajanviere@users.noreply.github.com>
2025-06-04 16:52:53 +0200

    chore: update dependencies and PostCSS configuration

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,10 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 export default {
   plugins: {
-    'tailwindcss': {},
-    'autoprefixer': {},
-    'postcss-preset-env': {
-      features: { 'nesting-rules': false },
-    },
+    tailwindcss: {},
+    autoprefixer: {},
   },
 };
